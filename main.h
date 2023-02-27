d2a4ab5922f49203cc5b34d3b04b726fcfaef328
grayolaRSA <gtndanga@gmail.com>
2023-02-27 19:40:59 +0200

    updates

--- main.h
+++ main.h
@@ -108,7 +108,7 @@ char buffer[],
 
 /****************** UTILS ******************/
 int is_printable(char);
-int append_hexa_code(char, char[], int);
+int append_hexa_code(unsigned char, char[], int);
 int is_digit(char);
 
 long int convert_size_number(long int num, int size);
