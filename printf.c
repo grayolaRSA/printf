9370a97e67cf786aaf831b723e951f8803290e73
grayolaRSA <gtndanga@gmail.com>
2023-02-27 19:32:11 +0200

    update

--- printf.c
+++ printf.c
@@ -1,112 +1,66 @@
 #include "main.h"
-#include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+
+void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
  */
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
-*_puts - prints strings
-*@s: string input
-*Return: integer output
-*/
-
-int _puts(char *s)
-{
-	int count = 0;
-
-	while (*s != '\0')
-	{
-		_putchar(*s);
-		s++;
-		count++;
-	}
-	return (count);
-}
-
-/**
- *print_int - prints integers
- *@n: integer
- *Return: number of characters
- */
-
-int print_int(int n)
-{
-	int count = 0;
-
-	if (n < 0)
-	{
-		_putchar('-');
-		n = -n;
-		count++;
-	}
-	if (n / 10)
-	{
-		count += print_int(n / 10);
-	}
-	_putchar(n % 10 + '0');
-	count++;
-	return (count);
-}
-
-/**
-*_printf - prints different types of string formats
-*@format: string input to function
-*Return: integer
-*/
-
 int _printf(const char *format, ...)
 {
-	int i, count = 0;
-	char error_msg[] = "Unknown format specifier: %";
-	va_list arg;
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
 
-	va_start(arg, format);
-	for (i = 0; format[i] != '\0'; i++)
+	if (format == NULL)
+		return (-1);
+
+	va_start(list, format);
+
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
-		if (format[i] == '%')
+		if (format[i] != '%')
 		{
-			i++;
-			switch (format[i])
-			{
-			case 'c':
-				count += _putchar(va_arg(arg, int));
-				break;
-
-			case 's':
-				count += _puts(va_arg(arg, char *));
-				break;
-
-			case '%':
-				count += _putchar('%');
-				break;
-			case 'i':
-			case 'd':
-				count += print_int(va_arg(arg, int));
-				break;
-			default:
-				_puts(error_msg);
-				_putchar(format[i]);
-				break;
-			}
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
 		}
 		else
-
-			_putchar(format[i]);
-
+		{
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
+		}
 	}
-	va_end(arg);
-	return (count);
+
+	print_buffer(buffer, &buff_ind);
+
+	va_end(list);
+
+	return (printed_chars);
+}
+
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
 }
